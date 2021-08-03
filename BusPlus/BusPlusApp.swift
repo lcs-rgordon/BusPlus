4b5ae03637b50acf62b3138995a60311cc55f569
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 15:34:28 -0400

    Initial Commit

---
+++ BusPlus/BusPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusPlusApp.swift
+//  BusPlus
+//
+//  Created by Russell Gordon on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct BusPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
